a8267228d35e4da9b24bf4048079e51431ac22d3
JacquesGM <jacquesgaldino@gmail.com>
2023-08-21 21:11:22 -0300

    hello.go new version

--- hello.go
+++ hello.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	name := "Jacques"
+	age := 36
+	version := 1.1
+	fmt.Println("Hello, Sr.", name, "your age is", age)
+	fmt.Println("This program is on version", version)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comand int
+	fmt.Scan(&comand)
+
+	fmt.Println("O endereço da minha variável é", &comand)
+	fmt.Println("O comando escolhido foi", comand)
+
 }
